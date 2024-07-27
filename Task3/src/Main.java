13599f2cdca1d39d72719d2996964a0d1ba4e4c8
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-27 11:58:47 +0300

    HW16 Task3 completed

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+
+    //public static final String VALID_STRING = " -АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+
     public static void main(String[] args) {
-        //todo ФИО
+        String input = getInput();
+        if(validateInput(input)) printFIO(input);
+    }
+
+    public static String getInput(){
+        System.out.println("Пожалуйста, введите свои фамилию, имя и отчество, разделенные пробелами: ");
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static boolean validateInput (String input){
+       boolean isValid = input.matches("([А-яёЁ-]+\\s){2}([А-яёЁ-]+)");
+       if (!isValid)
+            System.out.println("Введенная строка не является ФИО");
+       return isValid;
+    }
+
+    public static void printFIO(String input){
+        String[] fio = input.split(" ");
+        System.out.println("Фамилия: " + fio[0] + "\nИмя: " + fio[1] + "\nОтчество: " + fio[2]);
     }
 }
